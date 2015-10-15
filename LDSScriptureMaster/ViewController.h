fd65ae9e1a6ae37a3ae8c57e7df721db27fce955
James Moen <jamesmoen@yahoo.com>
2015-10-14 20:44:09 -0600

    Initial Commit

---
+++ LDSScriptureMaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LDSScriptureMaster
+//
+//  Created by Mac User on 10/14/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LDSScriptureMaster/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LDSScriptureMaster
+//
+//  Created by Mac User on 10/14/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
